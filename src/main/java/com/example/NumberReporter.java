7fed2fdd1814a3ec1b70b03818ea0f962f526335
ZhangZhao <zhaozhang@thoughtworks.com>
2021-04-08 23:32:29 +0800

    should return fizz/buzz/whizz when report given number divided 3/5/7

--- src/main/java/com/example/NumberReporter.java
+++ src/main/java/com/example/NumberReporter.java
@@ -2,6 +2,18 @@ package com.example;
 
 public class NumberReporter {
     public static String report(int number) {
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
+
+        if (number % 7 == 0) {
+            return "Whizz";
+        }
+
         return String.valueOf(number);
     }
 }
--- src/test/java/com/example/NumberReporterTest.java
+++ src/test/java/com/example/NumberReporterTest.java
@@ -10,4 +10,19 @@ public class NumberReporterTest {
     public void should_return_same_number_when_report_given_normal_number() {
         assertEquals(NumberReporter.report(1), "1");
     }
+
+    @Test
+    public void should_return_fizz_when_report_given_number_divided_3() {
+        assertEquals(NumberReporter.report(3), "Fizz");
+    }
+
+    @Test
+    public void should_return_buzz_when_report_given_number_divided_5() {
+        assertEquals(NumberReporter.report(5), "Buzz");
+    }
+
+    @Test
+    public void should_return_whizz_when_report_given_number_divided_7() {
+        assertEquals(NumberReporter.report(7), "Whizz");
+    }
 }
